ec091c47d682eb07e4d65867cb1a6aba25efd93b
rahulmaidenstride <130441295+rahulmaidenstride@users.noreply.github.com>
2024-05-08 13:36:40 +0530

    chnages 2 hr

---
+++ mla-new/about.php
@@ -0,0 +1,24 @@
+<?php include('header.php');?>
+
+
+
+
+<div class="dz-bnr-inr" style="background-image:url(images/background/bg-shape.jpg);">
+			<div class="container">
+				<div class="dz-bnr-inr-entry">
+					<h1>About us</h1>
+					<nav aria-label="breadcrumb" class="breadcrumb-row">
+						<ul class="breadcrumb">
+							<li class="breadcrumb-item"><a href="index.html"> Home</a></li>
+							<li class="breadcrumb-item">About us</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+
+
+
+
+
+<?php include('footer.php');?>
