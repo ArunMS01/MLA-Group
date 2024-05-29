fac46834c93c31f6f71c3bfff7c14c606030ff1d
rahulmaidenstride <130441295+rahulmaidenstride@users.noreply.github.com>
2024-05-29 19:19:04 +0530

    html blog post

---
+++ mla-new/blog-post.php
@@ -0,0 +1,172 @@
+<?php include('header.php')   ?> 
+
+<div class="page-content">
+		<!-- Blog Large -->
+		<section class="content-inner-1 bg-img-fix">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-xl-12 col-lg-12">
+						<!-- blog start -->
+						<div class="dz-blog blog-single style-1 sidebar">
+							<h1 class="dz-title">Uncovering the Beauty Benefits of Plant-Based Skincare</h1>
+							<div class="dz-meta">
+								<ul>
+									<li class="post-date">17 May 2022</li>
+									<li class="dz-user">
+										<i class="fa-solid fa-user"></i>
+										By <a href="javascript:void(0);">KK Sharma</a>
+									</li>
+									<li class="dz-comment">
+										<i class="fa-solid fa-message"></i>
+										<a href="javascript:void(0);">24 Comments</a>
+									</li>
+								</ul>
+							</div>
+							<div class="dz-media">
+								<img src="images/blog/detail/pic1.jpg" alt="/">
+							</div>
+							<div class="dz-info">
+								<div class="dz-post-text">
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+									<blockquote class="wp-block-quote is-style-default"><p>Create An Information Architecture That’s Easy To Use Way Precise Usability Considerations For Partially </p><cite>Ronald M. Spino</cite><svg width="64" height="46" viewBox="0 0 64 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M14.1943 0.412109C6.36815 0.412109 0.00195312 6.77831 0.00195312 14.6045C0.00195312 22.1543 5.92675 28.3466 13.3711 28.7744C13.5 30.166 13.4033 33.9541 9.77335 39.2236C9.49895 39.6211 9.54875 40.1572 9.88956 40.498C11.375 41.9834 12.293 42.9189 12.9355 43.5732C13.7764 44.4287 14.1602 44.8193 14.7217 45.3291C14.9121 45.5019 15.1523 45.5888 15.3936 45.5888C15.6279 45.5888 15.8613 45.5068 16.0498 45.3437C22.375 39.8398 29.4014 28.4677 28.3848 14.5322C27.7891 6.35061 21.8213 0.412109 14.1943 0.412109ZM15.4033 43.2226C15.1309 42.9541 14.8203 42.6377 14.3623 42.1718C13.8057 41.6045 13.042 40.8271 11.8838 39.665C16.2891 32.8769 15.457 28.042 15.0928 27.3486C14.9199 27.0195 14.5654 26.7978 14.1943 26.7978C7.47165 26.7978 2.00195 21.3281 2.00195 14.6045C2.00195 7.88181 7.47165 2.41211 14.1943 2.41211C20.7432 2.41211 25.8721 7.57031 26.3906 14.6767C27.5332 30.3506 18.2178 40.5273 15.4033 43.2226Z" fill="#24262B"></path>
+									<path d="M63.9004 14.5322C63.3027 6.35161 57.334 0.412109 49.709 0.412109C41.8828 0.412109 35.5156 6.77831 35.5156 14.6045C35.5156 22.1543 41.4414 28.3466 48.8867 28.7744C49.0156 30.165 48.9179 33.9511 45.2871 39.2236C45.0127 39.6211 45.0625 40.1572 45.4033 40.498C46.8828 41.9775 47.7988 42.9111 48.4414 43.5644C49.2861 44.4257 49.6719 44.8183 50.2363 45.33C50.4267 45.5019 50.6679 45.5888 50.9082 45.5888C51.1426 45.5888 51.376 45.5068 51.5644 45.3427C57.8896 39.8388 64.916 28.4668 63.9004 14.5322ZM50.9179 43.2226C50.6435 42.9521 50.3301 42.6338 49.8681 42.1631C49.3115 41.5966 48.5508 40.8213 47.3974 39.665C51.8027 32.8759 50.9717 28.042 50.6084 27.3486C50.4355 27.0205 50.0801 26.7978 49.709 26.7978C42.9853 26.7978 37.5156 21.3281 37.5156 14.6045C37.5156 7.88181 42.9853 2.41211 49.709 2.41211C56.2568 2.41211 61.3867 7.57031 61.9062 14.6777C63.0478 30.3496 53.7324 40.5273 50.9179 43.2226Z" fill="#24262B"></path>
+									</svg></blockquote>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+									<figure class="wp-container-5 wp-block-gallery-3 wp-block-gallery has-nested-images columns-3 is-cropped">
+										<figure class="wp-block-image size-large"><img src="images/blog/blogpost-3/pic1.jpg" alt="/"></figure>
+										<figure class="wp-block-image size-large"><img src="images/blog/blogpost-3/pic2.jpg" alt="/"></figure>
+										<figure class="wp-block-image size-large"><img src="images/blog/blogpost-3/pic3.jpg" alt="/"></figure>
+										<figure class="wp-block-image size-large"><img src="images/blog/blogpost-3/pic4.jpg" alt="/"></figure>
+										<figure class="wp-block-image size-large"><img src="images/blog/blogpost-3/pic1.jpg" alt="/"></figure>
+									</figure>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+									
+									<h3 class="dz-title">Additional information</h3>
+									<p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+									
+									<ul class="list-check-2 check-circle">
+										<li>Biodegradable, renewable bamboo handle</li>
+										<li>Charcoal infused nylon bristles</li>
+										<li>Designed to look awesome in your bathroom</li>
+										<li>Biodegradable, renewable bamboo handle</li>
+										<li>Charcoal infused nylon bristles</li>
+										<li>Designed to look awesome in your bathroom</li>
+									</ul>
+									
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+								</div>
+								<div class="dz-share-post meta-bottom">
+									<div class="post-tags">
+										<strong>Tags:</strong>
+										<a href="javascript:void(0);">Mats</a>
+										<a href="javascript:void(0);">Accessories</a>
+										<a href="javascript:void(0);">Bottles</a>
+										<a href="javascript:void(0);">Trackers</a>
+										<a href="javascript:void(0);">Bags</a>
+									</div>
+									<div class="dz-social-icon primary-light">
+										<ul>
+											<li><a class="fab fa-facebook-f" href="javascript:void(0);"></a></li>
+											<li><a class="fab fa-instagram" href="javascript:void(0);"></a></li>
+											<li><a class="fab fa-twitter" href="javascript:void(0);"></a></li>
+											<li><a class="fa-brands fa-linkedin-in" href="https://www.linkedin.com/showcase/3686700/admin/"></a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							
+						</div>
+						<div class="author-box style-1">
+							<div class="author-profile-info">
+								<div class="author-profile-pic">
+									<img src="images/blog/profile.jpg" alt="/">
+								</div>
+								<div class="author-profile-content">
+									<h6 class="title">I am John Doe</h6>
+									<p>We were making our way to the Rila Mountains, where we were visiting the Rila Monastery where we enjoyed scrambled eggs.</p>
+									<ul class="social-icon m-b0">
+										<li>
+											<a href="javascript:void(0);" target="_blank">
+												<i class="fa-brands fa-facebook-f"></i>
+											</a>
+										</li>
+										<li>
+											<a href="javascript:void(0);" target="_blank">
+												<i class="fa-brands fa-instagram"></i>
+											</a>
+										</li>
+										<li>
+											<a href="javascript:void(0);" target="_blank">
+												<i class="fa-brands fa-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a href="https://www.behance.net/HugeBinary" target="_blank">
+												<i class="fa-brands fa-behance"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="dz-related-post">
+							<h4 class="widget-title">Related Blogs</h4>
+							<div class="row">
+								<div class="col-lg-6 col-md-6 m-b30">
+									<div class="dz-card style-5 light">
+										<div class="dz-media">
+											<img src="images/blog/blogpost-5/pic1.jpg" alt="/">
+										</div>
+										<div class="dz-info">
+											<div class="dz-meta">
+												<ul>
+													<li class="post-date">20 Apr 2023</li>
+												</ul>
+											</div>
+											<h4 class="dz-title">
+												<a href="post-standard.html">Sink into Comfort with our Luxurious Couch Collection</a>
+											</h4>
+											<a href="post-standard.html" class="font-14 read-btn">Read More 
+												<i class="icon feather icon-chevron-right"></i>
+											</a>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-6 col-md-6 m-b30">
+									<div class="dz-card style-5 light">
+										<div class="dz-media">
+											<img src="images/blog/blogpost-5/pic2.jpg" alt="/">
+										</div>
+										<div class="dz-info">
+											<div class="dz-meta">
+												<ul>
+													<li class="post-date">10 June 2023</li>
+												</ul>
+											</div>
+											<h4 class="dz-title">
+												<a href="post-standard.html">Embrace Luxury Living with our Elegant Couches</a>
+											</h4>
+											<a href="post-standard.html" class="font-14 read-btn">Read More 
+												<i class="icon feather icon-chevron-right"></i>
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+									
+					
+						
+					</div>
+					
+				</div>
+			</div>
+		</section>
+		
+		<!-- Icon Box Start -->
+
+	<!-- Icon Box End -->
+	</div>
+
+<?php include('footer.php')   ?> 
