e48965793a01da2ba9e29e07ca260325b5cf4fd9
ArunMS01 <arun@maidenstride.com>
2026-06-16 12:29:03 +0530

    removed UA code and comments

--- head.php
+++ head.php
@@ -8,27 +8,19 @@
     <!-- FAVICONS ICON -->
     <link rel="icon" type="image/x-icon" href="https://www.mlagroup.com/images/logo-MLA.png">
 
-    <!-- PAGE TITLE HERE -->
-   
-
-    <!-- MOBILE SPECIFIC -->
+  
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- STYLESHEETS -->
-    <!--<link rel="stylesheet" type="text/css" href="vendor/bootstrap-select/dist/css/bootstrap-select.min.css">-->
+   
     <link rel="stylesheet" type="text/css" href="icons/fontawesome/css/all.min.css">
-    <!--<link rel="stylesheet" type="text/css" href="icons/themify/themify-icons.css">-->
-    <!--<link rel="stylesheet" type="text/css" href="icons/flaticon/flaticon_mooncart.css">-->
+   
     <link rel="stylesheet" type="text/css" href="vendor/swiper/swiper-bundle.min.css">
-    <!--<link rel="stylesheet" type="text/css" href="vendor/nouislider/nouislider.min.css">-->
+ 
     <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-    <!--<link rel="stylesheet" type="text/css" href="vendor/lightgallery/dist/css/lightgallery.css">-->
-    <!--<link rel="stylesheet" type="text/css" href="vendor/lightgallery/dist/css/lg-thumbnail.css">-->
-    <!--<link rel="stylesheet" type="text/css" href="vendor/lightgallery/dist/css/lg-zoom.css">-->
+    
     <link rel="stylesheet" type="text/css" href="css/style.css">
 
-    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>-->
-    <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>-->
+    
 
     <link rel="preconnect" href="https://fonts.googleapis.com/">
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
@@ -37,7 +29,6 @@
         rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/css/intlTelInput.css"/>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/intlTelInput.min.js"></script>
-
 
     <style>
      .header-nav .nav > li .mega-menu, .header-nav .nav > li .sub-menu{
@@ -57,15 +48,6 @@
 		} */
     </style>
     
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-178404512-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-178404512-1');
-</script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-EPE0XJ0VB4"></script>
 <script>
